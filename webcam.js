2ba5f4a3e795e1283f51d350998a6713c113900f
Kristoffer Nolgren <kirstoffer@nolgren.se>
2017-03-01 13:49:59 +0100

    .

--- webcam.js
+++ webcam.js
@@ -1,10 +1,11 @@
 // Triangel side length
+const text = document.getElementById('message')
 const right = document.createElement('canvas')
 const left = document.createElement('canvas')
 
 window.videoCanvas = {left: left, right: right}
 
-const placeImage = (canvas, dWidth, dHeight) => {
+const placeImage = (canvas, dWidth) => {
   const vc = canvas.getContext('2d')
   // Proportions
   const s = window.side
@@ -22,23 +23,27 @@ const placeImage = (canvas, dWidth, dHeight) => {
   vc.clip()
   vc.translate(canvas.width / 2, canvas.height / 2)
 
-  vc.scale(dWidth, dHeight)
+  vc.scale(dWidth, 1)
   vc.translate(-canvas.width / 2, -canvas.height / 2)
   vc.drawImage(ve,
     // Center image over clip
      -(ve.videoWidth - s) / 2,
      -(ve.videoHeight - s) / 2)
-  setTimeout(() => { placeImage(canvas, dWidth, dHeight) }, 33)
+  setTimeout(() => { placeImage(canvas, dWidth) }, 33)
 }
 
 const ve = document.createElement('video')
-navigator.mediaDevices.getUserMedia({video: {width: {ideal: 1280}, height: { ideal: 720 }}}).then((stream) => {
-  ve.src = window.URL.createObjectURL(stream)
-  ve.play()
-  placeImage(left, 1, 1)
-  placeImage(right, -1, 1)
-  document.body.appendChild(right)
-  document.body.appendChild(left)
+if (navigator.mediaDevices) {
+  text.innerHTML = 'This service require the use of your webcam, so please give it access. No data leaves your computer.'
+  navigator.mediaDevices.getUserMedia({video: {width: {ideal: 1280}, height: { ideal: 720 }}}).then((stream) => {
+    text.innerHTML = ''
 
-
-})
+    ve.src = window.URL.createObjectURL(stream)
+    ve.play()
+    placeImage(left, 1, 1)
+    placeImage(right, -1, 1)
+  })
+} else {
+  text.innerHTML = 'This service does not work in your browser, try firefox or Chrome.'
+}
+// else säg byt browser
